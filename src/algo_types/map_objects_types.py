3dd34199755517a2b9d3820c37d02b5db87424a2
Patrick ATTA-BAAH <patrickatta-baah@Patricks-Laptop.lan>
2024-09-17 10:20:42 -0400

    directory refactor

--- src/algo_types/map_objects_types.py
+++
@@ -1,9 +0,0 @@
-
-from typing import * 
-
-from dataclasses import dataclass, field, MISSING
-
-@dataclass
-class MapEntity: 
-    entity_id: str = field(default=MISSING, compare=True)
-    entity_type: str = field(default=MISSING)
